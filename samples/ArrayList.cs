a1235553fcee161c05644617878372225017d05c
artem.korotenko <artem.korotenko@betterme.world>
2022-01-11 08:26:44 +0200

    added list

---
+++ samples/ArrayList.cs
@@ -0,0 +1,52 @@
+﻿namespace Samples
+{
+    using System;
+
+    public class ArrayList
+    {
+        private int[] _array = new int[10];
+
+        private int _pointer = 0;
+
+        public void Add(int element)
+        {
+            _array[_pointer] = element;
+            _pointer += 1;
+
+            if (_pointer == _array.Length)
+            {
+                var extendedArray = new int[_array.Length * 2];
+                for (var i = 0; i < _array.Length; i++)
+                {
+                    extendedArray[i] = _array[i];
+                }
+
+                _array = extendedArray;
+                //this also can be achieved via
+                //Array.Resize(ref _array, _array.Length * 2);
+            }
+        }
+
+        public void Remove(int element)
+        {
+            for (var i = 0; i < _pointer; i++)
+            {
+                if (_array[i] == element)
+                {
+                    for (var j = i; j < _pointer - 1; j++)
+                    {
+                        _array[j] = _array[j + 1];
+                    }
+
+                    _pointer -= 1;
+                    return;
+                }
+            }
+        }
+
+        public int Count()
+        {
+            return _pointer;
+        }
+    }
+}
